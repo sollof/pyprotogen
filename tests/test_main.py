8ad58d0f028ea7392ee8d2f7238474cdc098e447
Sergei Soldatov <soldatov_sr@magnit.ru>
2023-03-20 20:43:07 +0300

    Add tests

---
+++ tests/test_main.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from typer.testing import CliRunner
+from pyprotogen import main
+from tempfile import TemporaryDirectory
+
+runner = CliRunner()
+
+PROTO_PATH = "./protos/hello.proto"
+GEN_DIR = 'genpack'
+
+
+def test_entrypoint() -> None:
+    with TemporaryDirectory() as tmpdir:
+        tmp_path = Path(tmpdir).joinpath(GEN_DIR)
+        result = runner.invoke(main.app, [PROTO_PATH, str(tmp_path.absolute())])
+
+    assert result.exit_code == 0
