fbea58953f8a8681750fe12202553580ea1bbdb7
Sergei Soldatov <soldatov_sr@magnit.ru>
2023-03-22 23:01:08 +0300

    Fix proto path procesing

--- pyprotogen/renderer.py
+++ pyprotogen/renderer.py
@@ -15,11 +15,23 @@ def gen_pb2_files(proto_path: str, output_path: str) -> None:
     grpc_gen_path = Path(output_path).joinpath(settings.GRPC_GEN_PATH)
     grpc_gen_path.mkdir(parents=True, exist_ok=True)
 
-    protoc.main((
-        '',
-        f'--proto_path={grpc_gen_path}={Path(proto_path).parent}',
-        '--python_out=./',
-        '--mypy_out=./',
-        '--grpc_python_out=./',
-        proto_path,
-    ))
+    proto_files = []
+    path = Path(proto_path)
+    if path.is_file():
+        proto_dir = path.parent
+        proto_files.append(str(path))
+    else:
+        proto_dir = path if path.is_dir() else path.parent
+        for f in proto_dir.iterdir():
+            if f.name.endswith('.proto'):
+                proto_files.append(str(f))
+
+    for proto_file in proto_files:
+        protoc.main((
+            '',
+            f'--proto_path={grpc_gen_path}={proto_dir}',
+            '--python_out=./',
+            '--mypy_out=./',
+            '--grpc_python_out=./',
+            str(proto_file),
+        ))
